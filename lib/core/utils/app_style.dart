5022d390dd1e55ac7bce468ac0ef1b95d0dae62f
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-06-11 20:47:12 +0300

    adding the app styles

---
+++ lib/core/utils/app_style.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/widgets.dart';
+
+abstract class AppStyle {
+  static const TextStyle textStyle18SimeBold = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle textStyle18Bold = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle textStyle18Medium = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle textStyle20Regular = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle textStyle20Bold = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle textStyle14Medium = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle textStyle14SimeBold = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle textStyle14Regular = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle textStyle30Regular = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle textStyle16Medium = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle textStyle16Bold = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+}
