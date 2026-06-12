cef6004e0e6dc75ab7422159cf8a9b2dfc61df15
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-06-12 19:58:48 +0300

    create book detail view and create route and make it navigate to the view

--- lib/core/routes/app_router_config.dart
+++ lib/core/routes/app_router_config.dart
@@ -1,9 +1,11 @@
+import 'package:bookly_app_with_clean_architure/feature/home/presentation/view/book_details_view.dart';
 import 'package:bookly_app_with_clean_architure/feature/home/presentation/view/home_view.dart';
 import 'package:bookly_app_with_clean_architure/feature/splash/presentation/view/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouterConfig {
   static const String homeViewRouter = "homeView";
+  static const String bookDetailViewRouter = "bookDetailView";
   static final GoRouter router = GoRouter(
     routes: [
       GoRoute(path: '/', builder: (context, state) => const SplashView()),
@@ -11,6 +13,11 @@ abstract class AppRouterConfig {
         name: homeViewRouter,
         path: '/$homeViewRouter',
         builder: (context, state) => const HomeView(),
+      ),
+      GoRoute(
+        name: bookDetailViewRouter,
+        path: '/$bookDetailViewRouter',
+        builder: (context, state) => const BookDetailsView(),
       ),
     ],
   );
--- lib/feature/home/presentation/view/widgets/best_seller_item.dart
+++ lib/feature/home/presentation/view/widgets/best_seller_item.dart
@@ -1,51 +1,58 @@
+import 'package:bookly_app_with_clean_architure/core/routes/app_router_config.dart';
 import 'package:bookly_app_with_clean_architure/core/utils/app_style.dart';
 import 'package:bookly_app_with_clean_architure/feature/home/presentation/view/widgets/feature_image_item.dart';
 import 'package:bookly_app_with_clean_architure/feature/home/presentation/view/widgets/rating_best_seller.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class BestSellerItem extends StatelessWidget {
   const BestSellerItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 120,
-      child: Row(
-        children: [
-          const FeatureImageItem(),
-          const SizedBox(width: 30),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Harry Potter and the Goblet of Fire",
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 2,
-                  style: AppStyle.textStyle20Regular.copyWith(
-                    color: Colors.white,
+    return GestureDetector(
+      onTap: () {
+        context.pushNamed(AppRouterConfig.bookDetailViewRouter);
+      },
+      child: SizedBox(
+        height: 120,
+        child: Row(
+          children: [
+            const FeatureImageItem(),
+            const SizedBox(width: 30),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Harry Potter and the Goblet of Fire",
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                    style: AppStyle.textStyle20Regular.copyWith(
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 3),
-                Text(
-                  "J.K. Rowling",
-                  style: AppStyle.textStyle14Medium.copyWith(
-                    color: const Color.fromARGB(255, 168, 164, 164),
+                  const SizedBox(height: 3),
+                  Text(
+                    "J.K. Rowling",
+                    style: AppStyle.textStyle14Medium.copyWith(
+                      color: const Color.fromARGB(255, 168, 164, 164),
+                    ),
                   ),
-                ),
-                const SizedBox(height: 3),
-                const Row(
-                  children: [
-                    Text("19.99 €", style: AppStyle.textStyle20Bold),
-                    Spacer(),
-                    RatingBestSeller(),
-                  ],
-                ),
-              ],
+                  const SizedBox(height: 3),
+                  const Row(
+                    children: [
+                      Text("19.99 €", style: AppStyle.textStyle20Bold),
+                      Spacer(),
+                      RatingBestSeller(),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
