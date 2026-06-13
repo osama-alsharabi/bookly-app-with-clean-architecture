23f992d7c5f3ecd1e372b9ab22af6222fb33f4d2
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-06-13 23:01:35 +0300

    create the book entity

---
+++ lib/feature/home/domain/entities/book_entity.dart
@@ -0,0 +1,13 @@
+class BookEntity {
+  final String image;
+  final String title;
+  final String author;
+  final num price;
+
+  BookEntity({
+    required this.image,
+    required this.title,
+    required this.author,
+    required this.price,
+  });
+}
