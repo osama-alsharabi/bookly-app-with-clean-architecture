86274b1712405f86078a739e906928206371b690
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-06-14 23:52:47 +0300

    create the remote data source contract

---
+++ lib/feature/home/data/data_sources/home_remote_data_source.dart
@@ -0,0 +1,6 @@
+import 'package:bookly_app_with_clean_architure/feature/home/domain/entities/book_entity.dart';
+
+abstract class HomeRemoteDataSource {
+  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
+}
