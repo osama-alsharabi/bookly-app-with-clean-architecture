ea73664e8f6dde3e2553b897d14e8d67e0056564
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-06-16 00:03:45 +0300

    create the bloc provider of the fetch newest  books cubit

--- lib/feature/home/presentation/view/home_view.dart
+++ lib/feature/home/presentation/view/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app_with_clean_architure/core/services/service_locator.dart';
 import 'package:bookly_app_with_clean_architure/feature/home/presentation/view/widgets/home_view_body.dart';
 import 'package:bookly_app_with_clean_architure/feature/home/presentation/view_model/fetch_featured_books/fetch_featured_books_cubit.dart';
+import 'package:bookly_app_with_clean_architure/feature/home/presentation/view_model/fetch_newest_books/fetch_newest_books_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -9,9 +10,17 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) =>
-          getIt.get<FetchFeaturedBooksCubit>()..fetchFeaturedBooks(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) =>
+              getIt.get<FetchFeaturedBooksCubit>()..fetchFeaturedBooks(),
+        ),
+        BlocProvider(
+          create: (context) =>
+              getIt.get<FetchNewestBooksCubit>()..fetchNewsetBooks(),
+        ),
+      ],
       child: const Scaffold(body: SafeArea(child: HomeViewBody())),
     );
   }
