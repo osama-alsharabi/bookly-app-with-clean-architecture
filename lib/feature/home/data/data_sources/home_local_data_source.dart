0cfe52f2562e0c585f5c57cd070a5a9a8248b492
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-06-15 16:41:08 +0300

    create cache books in the interface home local data source

--- lib/feature/home/data/data_sources/home_local_data_source.dart
+++ lib/feature/home/data/data_sources/home_local_data_source.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app_with_clean_architure/feature/home/data/model/book_model/book_model.dart';
 
 abstract class HomeLocalDataSource {
+  cacheBooks({required String boxName});
   List<BookModel> getFeaturedBooks();
   List<BookModel> getNewestBooks();
 }
