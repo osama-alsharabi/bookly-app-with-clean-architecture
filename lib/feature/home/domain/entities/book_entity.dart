e0ffe820eebf88fb06808fee0b7f6fa2b80493ae
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-06-15 16:14:50 +0300

    done make the book entity adapter

--- lib/feature/home/domain/entities/book_entity.dart
+++ lib/feature/home/domain/entities/book_entity.dart
@@ -1,8 +1,17 @@
+import 'package:hive/hive.dart';
+part 'book_entity.g.dart';
+
+@HiveType(typeId: 1)
 class BookEntity {
+  @HiveField(0)
   final String image;
+  @HiveField(1)
   final String title;
+  @HiveField(2)
   final String author;
+  @HiveField(3)
   final num price;
+  @HiveField(4)
   final num rating;
 
   BookEntity({
