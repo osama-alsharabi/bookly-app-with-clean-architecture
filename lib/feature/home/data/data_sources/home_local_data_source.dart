289994a99e21d74edebadb0f35e2b17648c6c07f
Osama Alsharabi <osamaahmed70088@gmail.com>
2026-06-15 15:34:46 +0300

    create the get featured books and make the class abstract

--- lib/feature/home/data/data_sources/home_local_data_source.dart
+++ lib/feature/home/data/data_sources/home_local_data_source.dart
@@ -1,1 +1,5 @@
-class HomeLocalDataSource {}
+import 'package:bookly_app_with_clean_architure/feature/home/data/model/book_model/book_model.dart';
+
+abstract class HomeLocalDataSource {
+  List<BookModel> getFeaturedBooks();
+}
